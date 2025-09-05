f0ab795cb3049c0af755a788cade5930f675860f
Lgy <lgylgy1120@naver.com>
2025-09-05 16:27:08 +0900

    백앤드 배포 수정

--- src/main/java/com/bmm/api/logIn/security/JwtAuthenticationFilter.java
+++ src/main/java/com/bmm/api/logIn/security/JwtAuthenticationFilter.java
@@ -50,7 +50,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 									HttpServletResponse response,
 									FilterChain filterChain) throws ServletException, IOException {
 		// CORS 응답 헤더 설정
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:5174");
+       // response.setHeader("Access-Control-Allow-Origin", "http://localhost:5174");
+	    response.setHeader("Access-Control-Allow-Origin", "https://calm-stone-09e440100.1.azurestaticapps.net");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
         response.setHeader("Access-Control-Allow-Credentials", "true");
